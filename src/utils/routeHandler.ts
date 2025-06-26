91335044ce75e31ecf5894c75e84299ec0cb7fcf
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-06-26 11:48:19 +0900

    routeのリファクタリング（ヘルパー関数を作成）(PR指摘対応)

--- src/utils/routeHandler.ts
+++ src/utils/routeHandler.ts
@@ -1,20 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import { container } from "tsyringe";
-import { constructor } from "tsyringe/dist/typings/types";
+import { constructor as tSyringeConstructor } from "tsyringe/dist/typings/types";
+import { AppError } from "../middlewares/errorMiddleware";
 
 // コントローラーのメソッドの型を定義
 type ControllerMethod = (req: Request, res: Response) => Promise<void | Response>
 
 export function createHandler<T>(
-    controller: constructor<T>,
+    controller: tSyringeConstructor<T>,
     methodName: keyof T
 ) {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
             const controllerInstance = container.resolve<T>(controller)
             // メソッドを安全にキャスト
-            const method = controllerInstance[methodName] as unknown as ControllerMethod
-            method.call(controllerInstance, req, res).catch(next)
+            const method = controllerInstance[methodName]
+            if (typeof method === 'function') {
+                const result = (method as ControllerMethod).call(controllerInstance, req, res)
+                if (result && typeof result.catch === 'function') {
+                    result.catch(next)
+                }
+            } else {
+                next(new AppError(`${String(methodName)}はコントローラに存在しません`, 500))
+            }
         } catch (error) {
             next(error)
         }
